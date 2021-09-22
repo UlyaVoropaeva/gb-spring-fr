5f1f27c922a8ce712946386ffd61bf6242e45a1b
Lenovo-PC <Ulya2811@yandex.ru>
2021-09-22 16:57:28 +0300

    lesson_7

---
+++ src/main/java/ru/servise/ProductService.java
@@ -0,0 +1,24 @@
+package ru.servise;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.domain.Product;
+import java.util.List;
+
+
+public interface ProductService  {
+
+    List<Product> findAll();
+
+    Product getById(Long id);
+
+    void saveOrUpdate(Product product);
+
+    void deleteById(Long id);
+
+    Product update(Long id, Product product);
+
+    List<Product> filterByPriceMin();
+    List<Product> filterByPriceMax();
+    List<Product> filterByPriceMinMax();
+}
---
+++ src/main/java/ru/servise/ProductServiceImpl.java
@@ -0,0 +1,63 @@
+package ru.servise;
+
+import org.springframework.stereotype.Service;
+import ru.dao.ProductDao;
+import ru.domain.Product;
+
+import javax.ejb.EJB;
+import javax.ejb.TransactionAttribute;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    @EJB
+    private ProductDao productDao;
+
+    @Override
+    public List<Product> findAll() {
+        return productDao.findAll();
+    }
+
+    @Override
+    public Product getById(Long id) {
+        Product product = productDao.getById(id);
+        if (product != null) {
+            return product;
+        }
+        return null;
+    }
+
+    @TransactionAttribute
+    @Override
+    public void saveOrUpdate(Product product) {
+
+        productDao.saveOrUpdate(product);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productDao.deleteById(id);
+
+    }
+
+    @Override
+    public Product update(Long id, Product product) {
+        return productDao.update(id,product);
+    }
+
+    @Override
+    public List<Product> filterByPriceMin() {
+        return productDao.filterByPriceMin();
+    }
+
+    @Override
+    public List<Product> filterByPriceMax() {
+        return productDao.filterByPriceMax();
+    }
+
+    @Override
+    public List<Product> filterByPriceMinMax() {
+        return productDao.filterByPriceMinMax();
+    }
+}
